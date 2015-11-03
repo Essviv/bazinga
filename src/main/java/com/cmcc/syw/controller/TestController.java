778f9e99db3eec22f12d433f962b9548302a0af1
Essviv <514912821@qq.com>
2015-11-03 12:08:34 +0800

    1. add persistent support 2. add test controller

---
+++ src/main/java/com/cmcc/syw/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.cmcc.syw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sunyiwei on 2015/11/3.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("index")
+    public String test(){
+        return "index";
+    }
+}
